b1a3f322e6a362b4269e84f01109052be4f92a9a
Ashwani Arya <ashwaniarya2009@gmail.com>
2025-08-03 16:12:00 +0530

    Aug 3

--- Advanced Track/Aug 3/Problem1.java
+++ Advanced Track/Aug 3/Problem1.java
@@ -2,6 +2,17 @@ import java.util.Scanner;
 
 public class Problem1 {
     public static void main(String[] args) {
+
+        // Q) Nearly Lucky Number
+
+        // A "lucky number" is a positive integer whose digits are only 4 or 7 (e.g., 4,
+        // 7, 44, 747).
+        // A number is "nearly lucky" if the count of its lucky digits (4 or 7) is
+        // itself a lucky number.
+
+        // You are given a (possibly very large) integer n, and must check if n is a
+        // nearly lucky number.
+
         Scanner sc = new Scanner(System.in);
         long n = sc.nextLong();
         int count = 0;
--- Advanced Track/Aug 3/Problem2.java
+++ Advanced Track/Aug 3/Problem2.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 
 public class Problem2 {
     public static void main(String[] args) {
+        //Q) Helpful Maths
+
+        // You are given a string representing a sum of numbers using only 1, 2, and 3
+        // (e.g., "3+2+1"), with numbers separated by plus signs. Your task is to
+        // rearrange the summands so that the numbers appear in non-decreasing order
+        // (smallest to largest), while keeping the numbers separated by plus signs.
+        // This will help Xenia add them more easily.
+
+        // Example:
+        // Input: 3+2+1
+        // Output: 1+2+3
+        
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         ArrayList<Integer> arr = new ArrayList<>();
@@ -12,11 +24,11 @@ public class Problem2 {
             }
             arr.add(Character.getNumericValue(str.charAt(i)));
         }
-        arr.sort((a,b)->a-b);
-        for(int i = 0; i<arr.size()-1;i++){
-            System.out.print(arr.get(i)+"+");
+        arr.sort((a, b) -> a - b);
+        for (int i = 0; i < arr.size() - 1; i++) {
+            System.out.print(arr.get(i) + "+");
         }
-        System.out.println(arr.get(arr.size()-1));
+        System.out.println(arr.get(arr.size() - 1));
         sc.close();
     }
 }
